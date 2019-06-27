4ab33262176fc880a476a905e8e3789180b1703e
eunha0ne <eunha0ne@gmail.com>
2019-06-27 15:24:13 +0900

    fixed (#23) No Effect for Nested Component

--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,38 @@ function getPosts() {
   ];
 }
 
+const PostLink = ({ post }) => (
+  <li>
+    <Link as={`/${post}`} href={`/post?title=${post.title}`}>
+      <a>{post.title}</a>
+    </Link>
+    <style jsx>{`
+    li {
+      list-style: none;
+      margin: 5px 0;
+    }
+
+    a {
+      text-decoration: none;
+      color: blue;
+      font-family: 'Arial';
+    }
+
+    a:hover {
+      opacity: 0.6
+    }
+    `}</style>
+  </li>
+
+);
+
 export default function Blog() {
   return (
     <Layout>
       <h1>Next Blog</h1>
       <ul>
         {getPosts().map(post => (
-          <li key={post.id}>
-            <Link as={`/${post}`} href={`/post?title=${post.title}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
+          <PostLink key={post.id} post={post} />
         ))}
       </ul>
       <style jsx>{`
@@ -29,17 +50,6 @@ export default function Blog() {
       }
       ul {
         padding: 0
-      }
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-      a:hover {
-        opacity: 0.6
       }
       `}</style>
     </Layout>
