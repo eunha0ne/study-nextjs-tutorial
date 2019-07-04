baa12caadf3cbedd4018f6eae2e1bf833e30b7af
eunha0ne <eunha0ne@gmail.com>
2019-07-04 18:25:34 +0900

    deploying nextjs app

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
+/*
 // Method 1 - Layout as a Higher Order Component
 import withLayout from '../components/Layout';
 
+
 const Page = () => <p>This is the about page.</p>;
 export default withLayout(Page);
+*/
 
 // Method 2 - Page content as a prop
-/*
 import Layout from '../components/Layout';
 
 export default function About() {
@@ -15,4 +17,3 @@ export default function About() {
     </Layout>
   )
 }
-*/
